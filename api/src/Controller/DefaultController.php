cab4cb25c9360d2859aaaf0b60700f19f6d6f007
Dawid <dannn.dawid@gmail.com>
2021-10-15 19:05:17 +0200

    Fast push

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -2,12 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class DefaultController extends AbstractController
 {
+    private UserPasswordEncoderInterface $authenticatedUser;
+
+    private UserRepository $repo;
+
+    public function __construct
+    (
+        UserPasswordEncoderInterface $authenticatedUser,
+        UserRepository $userRepository
+    )
+    {
+        $this->authenticatedUser = $authenticatedUser;
+        $this->repo = $userRepository;
+    }
+
     #[Route('/default', name: 'default')]
     public function index(): Response
     {
@@ -15,5 +32,17 @@ class DefaultController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/DefaultController.php',
         ]);
+    }
+
+    #[Route('/register', name: 'register', methods: ['POST'])]
+    public function registerUser()
+    {
+        $user = new User();
+        $user   ->setEmail('dawid@gmail.com')
+                ->setPassword($this->authenticatedUser->encodePassword($user, 'haslo'));
+
+        $this->repo->save($user);
+
+        return new Response("Chujj Ci w dupe");
     }
 }
--- api/src/Repository/UserRepository.php
+++ api/src/Repository/UserRepository.php
@@ -5,18 +5,30 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method UserInterface loadUserByIdentifier(string $identifier)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository implements UserProviderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
+    }
+
+    public function save(User $user)
+    {
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
     }
 
     // /**
@@ -47,4 +59,41 @@ class UserRepository extends ServiceEntityRepository
         ;
     }
     */
+
+    public function findOneByEmail(string $username): ?User
+    {
+        return $this->findOneBy(['email' => $username]);
+    }
+
+    private function findUsername(string $username): User
+    {
+        $user = $this->findOneByEmail($username);
+        if ($user !== null) {
+            return $user;
+        }
+        throw new UsernameNotFoundException(
+            sprintf('Username "%s" does not exist.', $username)
+        );
+    }
+
+    public function refreshUser(UserInterface $user): UserInterface
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', \get_class($user))
+            );
+        }
+        $username = $user->getUsername();
+        return $this->findUsername($username);
+    }
+
+    public function supportsClass(string $class): bool
+    {
+        return User::class === $class;
+    }
+
+    public function loadUserByUsername(string $username): UserInterface
+    {
+        return $this->findUsername($username);
+    }
 }
