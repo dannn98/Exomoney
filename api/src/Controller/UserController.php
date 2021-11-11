af69f04248c7031ad7fd843e789b8036a16027ab
Dawid <dannn.dawid@gmail.com>
2021-11-11 15:08:39 +0100

    Create user methods

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -2,46 +2,40 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Repository\UserRepository;
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
-    private UserPasswordEncoderInterface $authenticatedUser;
-    private UserRepository $repo;
+    private UserService $userService;
 
+    /**
+     * @param UserService $userService
+     */
     public function __construct
     (
-        UserPasswordEncoderInterface $authenticatedUser,
-        UserRepository               $userRepository
+        UserService $userService
     )
     {
-        $this->authenticatedUser = $authenticatedUser;
-        $this->repo = $userRepository;
+        $this->userService = $userService;
     }
 
     /**
      * @param Request $request
-     * @return Response
-     * @throws ORMException
-     * @throws OptimisticLockException
+     * @return JsonResponse
      */
-    #[Route('/register', name: 'register', methods: ['POST'])]
-    public function registerUser(Request $request): Response
+    #[Route('/user/create', name: 'user/create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
-        $user = new User();
-        $user->setEmail($data['email'])->setPassword($this->authenticatedUser->encodePassword($user, $data['password']));
 
-        $this->repo->save($user);
+        if($this->userService->createUser($data)) {
+            return new JsonResponse(['message' => 'Dodano użytkownika'], 200, []);
+        }
 
-        return new Response(true, 200, []);
+        return new JsonResponse(['message' => 'Wystąpił błąd'], 409, []);
     }
 }
---
+++ api/src/Service/UserService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserService
+{
+    private UserPasswordEncoderInterface $authenticatedUser;
+    private UserRepository $userRepository;
+
+    /**
+     * @param UserPasswordEncoderInterface $authenticatedUser
+     */
+    public function __construct(
+        UserPasswordEncoderInterface $authenticatedUser,
+        UserRepository $userRepository
+    )
+    {
+        $this->authenticatedUser = $authenticatedUser;
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @param array $data
+     * @return bool
+     */
+    public function createUser(array $data): bool
+    {
+        $user = new User();
+        $user->setEmail($data['email'])->setPassword($this->authenticatedUser->encodePassword($user, $data['password']));
+
+        try {
+            //TODO: Walidacja user
+            $this->userRepository->save($user);
+        } catch (OptimisticLockException | ORMException $e) {
+            dump($e->getCode() . ": " . $e->getMessage());
+            return false;
+        }
+
+        return true;
+    }
+}
