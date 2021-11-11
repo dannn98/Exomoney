54671b25ed78d765ee3ebc993a8b0e204196dbdb
Dawid <dannn.dawid@gmail.com>
2021-11-11 20:45:43 +0100

    UserController refactoring

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -6,8 +6,10 @@ use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route(path: '/user', name: 'user.')]
 class UserController extends AbstractController
 {
     private UserService $userService;
@@ -15,8 +17,7 @@ class UserController extends AbstractController
     /**
      * @param UserService $userService
      */
-    public function __construct
-    (
+    public function __construct(
         UserService $userService
     )
     {
@@ -27,15 +28,15 @@ class UserController extends AbstractController
      * @param Request $request
      * @return JsonResponse
      */
-    #[Route('/user/create', name: 'user/create', methods: ['POST'])]
+    #[Route(name: 'create', methods: ['POST'])]
     public function create(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
         if ($this->userService->createUser($data)) {
-            return new JsonResponse(['message' => 'Dodano użytkownika'], 200, []);
+            return new JsonResponse(['message' => 'Dodano użytkownika'], Response::HTTP_CREATED, []);
         }
 
-        return new JsonResponse(['message' => 'Wystąpił błąd'], 409, []);
+        return new JsonResponse(['message' => 'Wystąpił błąd'], Response::HTTP_CONFLICT, []);
     }
 }
