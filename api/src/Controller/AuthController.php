aabc044b9c2e937ed86e128ec977119e6c55a549
Dawid <dannn.dawid@gmail.com>
2021-11-11 20:46:26 +0100

    OAuth2, firewalls configuration

--- api/src/Controller/AuthController.php
+++ api/src/Controller/AuthController.php
@@ -2,42 +2,110 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Repository\UserRepository;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+#[Route(path: '/auth', name: 'auth.')]
 class AuthController extends AbstractController
 {
-    private UserPasswordEncoderInterface $authenticatedUser;
-    private UserRepository $repo;
+    private PsrHttpFactory $psrHttpFactory;
 
-    public function __construct
-    (
-        UserPasswordEncoderInterface $authenticatedUser,
-        UserRepository               $userRepository
+    public function __construct(
+        PsrHttpFactory $psrHttpFactory
     )
     {
-        $this->authenticatedUser = $authenticatedUser;
-        $this->repo = $userRepository;
+        $this->psrHttpFactory = $psrHttpFactory;
     }
 
     /**
      * @param Request $request
-     * @return Response
+     * @return JsonResponse
      */
     #[Route('/login', name: 'login', methods: ['POST'])]
-    public function loginUser(Request $request): Response
+    public function login(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
-        $user = new User();
-        $user->setEmail($data['email'])
-            ->setPassword($this->authenticatedUser->encodePassword($user, $data['password']));
+        $psrRequest = $this->psrHttpFactory->createRequest($request);
 
-        return new Response(true, 200, []);
+        $result = $this->requestOauth([
+            'username' => $data['email'],
+            'password' => $data['password'],
+            'grant_type' => 'password'
+        ], $psrRequest);
+
+        if ($result->getStatusCode() == '200') {
+            $responseData = json_decode($result->getContent());
+
+            return $this->respondWithToken($responseData);
+        }
+
+        return new JsonResponse(['message' => 'Logowanie nie powiodło się'], 400, []);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    #[Route('/refresh-token', name: 'refresh-token', methods: ['POST'])]
+    public function refreshToken(Request $request): JsonResponse
+    {
+        $refreshToken = $request->cookies->get('refresh_token');
+
+        $psrRequest = $this->psrHttpFactory->createRequest($request);
+
+        $result = $this->requestOauth([
+            'refresh_token' => $refreshToken,
+            'grant_type' => 'refresh_token',
+            'scope' => ''
+        ], $psrRequest);
+
+        if ($result->getStatusCode() == '200') {
+            $responseData = json_decode($result->getContent());
+
+            return $this->respondWithToken($responseData);
+        }
+
+        return new JsonResponse(['message' =>'Refresh token failed'], 401, []);
+    }
+
+    protected function requestOauth(array $data, ServerRequestInterface $request): Response
+    {
+        $data = array_merge($data, [
+            'client_id' => $this->getParameter('oauth2_client_id'),
+            'client_secret' => $this->getParameter('oauth2_secret'),
+        ]);
+
+        return $this->forward($this->routeToControllerName(), ['serverRequest' => $request->withParsedBody($data)]);
+    }
+
+    protected function respondWithToken($responseData): JsonResponse
+    {
+        $response = new JsonResponse([
+            'token_type' => $responseData->token_type,
+            'access_token' => $responseData->access_token,
+            'expires_in' => $responseData->expires_in,
+        ], 200);
+        $response->headers->setCookie(
+            Cookie::create('refresh_token')
+                ->withValue($responseData->refresh_token)
+                ->withExpires(864000)
+                ->withDomain('.example.com')
+                ->withSecure(true)
+        );
+
+        return $response;
+    }
+
+    private function routeToControllerName(): mixed
+    {
+        $routes = $this->get('router')->getRouteCollection();
+        return $routes->get('oauth2_token')->getDefaults()['_controller'];
     }
 }
