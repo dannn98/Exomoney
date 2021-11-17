3f20cdbe89e6d7badc01576a5a6e02a081130cdf
Dawid <dannn.dawid@gmail.com>
2021-11-17 14:42:23 +0100

    Add many-to-many user_team relation

--- api/src/Entity/Team.php
+++ api/src/Entity/Team.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\TeamRepository;
 use App\Traits\CreatedAt;
 use App\Traits\ModifiedAt;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -35,6 +37,16 @@ class Team
      * @ORM\Column(type="string", length=1024)
      */
     private string $avatarUrl;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=User::class, mappedBy="teams")
+     */
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
 
     //Traits
     use CreatedAt;
@@ -77,5 +89,32 @@ class Team
     public function setAvatarUrl(string $avatarUrl): void
     {
         $this->avatarUrl = $avatarUrl;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->addTeam($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->removeElement($user)) {
+            $user->removeTeam($this);
+        }
+
+        return $this;
     }
 }
--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use App\Traits\CreatedAt;
 use App\Traits\ModifiedAt;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -37,6 +39,16 @@ class User implements UserInterface
      * @ORM\Column(type="string", length=255)
      */
     private ?string $password;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Team::class, inversedBy="users")
+     */
+    private $teams;
+
+    public function __construct()
+    {
+        $this->teams = new ArrayCollection();
+    }
 
     //Traits
     use CreatedAt;
@@ -128,6 +140,30 @@ class User implements UserInterface
     public function setPassword(string $password): self
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Team[]
+     */
+    public function getTeams(): Collection
+    {
+        return $this->teams;
+    }
+
+    public function addTeam(Team $team): self
+    {
+        if (!$this->teams->contains($team)) {
+            $this->teams[] = $team;
+        }
+
+        return $this;
+    }
+
+    public function removeTeam(Team $team): self
+    {
+        $this->teams->removeElement($team);
 
         return $this;
     }
