aad8fc6f09592bd75459ce9ecf689de917e13eb1
dawid.dabek <dawid.dabek@firma.interia.pl>
2022-01-10 02:43:35 +0100

    Add team info, debts and repayments to TeamView

---
+++ ui/front/src/components/RepaymentListElem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class='repayment-list-elem pointer'>
+        <p>{{target.nickname}} ({{value}})</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RepaymentListElem',
+    components: {},
+    data() {
+
+    },
+    props: {
+        uid: {required: true, type: String},
+        user: {required: true, type: Object},
+        target: {required: true, type: Object},
+        value: {required: true, type: String},
+    }
+}
+</script>
+
+<style scoped>
+    .repayment-list-elem {
+        width: 100%;
+        height: 35px;
+
+        margin-bottom: 3px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        font-size: 13px;
+        font-weight: 100;
+
+        color: whitesmoke;
+
+        background-color: rgba(0, 0, 0, 0.15);
+    }
+</style>
